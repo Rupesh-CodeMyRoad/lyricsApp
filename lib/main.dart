8d97d53431b386aee356acf759a11e8305bb2e77
Rupesh <rupesh.gaudel@infodevelopers.com.np>
2023-01-01 12:40:14 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'My Anthems',
+    home: Text('My App'),
+    debugShowCheckedModeBanner: false,
+  ));
+}
