03bde9158003d1949f53d46f69e6d4009851a4b1
Rupesh <rupesh.gaudel@infodevelopers.com.np>
2023-01-02 16:32:14 +0545

    screens created

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:lyrics_app/screens/app.dart';
 
 void main() {
   runApp(MaterialApp(
     title: 'My Anthems',
-    home: Text('My App'),
     debugShowCheckedModeBanner: false,
+    home: MyApp(),
   ));
 }
--- lib/screens/app.dart
+++ lib/screens/app.dart
@@ -1,16 +1,55 @@
-// import 'package:flutter/src/widgets/container.dart';
-// import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:lyrics_app/screens/home.dart';
+import 'package:lyrics_app/screens/search.dart';
+import 'package:lyrics_app/screens/yourlib.dart';
 
-// class MyApp extends StatefulWidget {
-//   const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
-//   @override
-//   State<MyApp> createState() => _MyAppState();
-// }
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
-// class _MyAppState extends State<MyApp> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container();
-//   }
-// }
+class _MyAppState extends State<MyApp> {
+  final tabs = [Home(), Search(), Library()];
+  int currentTabIndex = 0;
+//All UI design go inside build
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: tabs[currentTabIndex],
+      backgroundColor: Colors.black,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentTabIndex,
+        onTap: (value) {
+          print("Current Index is $value");
+          currentTabIndex = value;
+          setState(() {});
+        },
+        selectedLabelStyle: TextStyle(color: Colors.white),
+        backgroundColor: Colors.black45,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Colors.white,
+              ),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              label: 'search'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.library_books,
+                color: Colors.white,
+              ),
+              label: 'library')
+        ],
+      ),
+    );
+  }
+}
